2b7b3f479ef7c16578c143072558d569284a3b2f
JavierDeSant <j.desantiagosoto@gmail.com>
2024-09-04 17:04:45 +0200

    feat!: improve navigation by using only one fn feat!: use zod for validation and error handling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,7 +3,7 @@ import { Controller, useForm } from "react-hook-form"
 import { z } from "zod" // TODO: implement zod for safer validation
 import { zodResolver } from "@hookform/resolvers/zod"
 import 'react-phone-number-input/style.css'
-import PhoneInput, { isValidPhoneNumber } from 'react-phone-number-input'
+import PhoneInput from 'react-phone-number-input'
 import { formDataSchema } from '../lib/schema.ts'
 
 interface FormProps {}
@@ -60,25 +60,25 @@ export default function Form({}: FormProps) {
 
     const onSubmit = useCallback((values: FormValues) => {
         window.alert(JSON.stringify(values, null, 4))
+        handleNav(currentStep + 1)
     }, [])
 
-    const next = async (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-        const isValid = await trigger(steps[currentStep].fields)
+    const handleNav = async (index: number) => {
+        let isValid = true;
+        let failedStep = currentStep;
+        for (let i = currentStep; i <= index && isValid; i++) {
+            isValid = await trigger(steps[i].fields)
+            if (!isValid) {
+                failedStep = i
+            }
+        }
 
-        if (!isValid) return
-
-        if (currentStep < steps.length - 1) {
-            setCurrentStep(step => step + 1)
+        if (isValid) {
+            setCurrentStep(index)
+        } else {
+            setCurrentStep(failedStep)
         }
     }
-
-    const prev = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-        if (currentStep > 0) {
-            setCurrentStep(prevStep => prevStep - 1);
-        }
-    };
 
     return ( 
         <div className="flex grow bg-white p-5">
@@ -87,9 +87,10 @@ export default function Form({}: FormProps) {
             <div className="flex flex-col w-[274px] min-h-[568px] shrink-0 p-5 bg-sidebar-desktop rounded-xl">
                 {steps.slice(0, -1).map((step, index) => (
                     <div className="flex ml-2 my-4 items-center" key={step.id}>
-                        <button id="step-1" 
+                        <button id={step.id}
+                            disabled={ `step-${currentStep + 1}` === step.id }
                             className={`h-8 w-8 border font-bold text-sm rounded-full ${currentStep === index ? "bg-brand-pastel-blue text-brand-marine-blue" : "text-brand-alabaster" }`}
-                            onClick={() => setCurrentStep(index)}
+                            onClick={() => handleNav(index)}
                         >{index+1}</button>
                         <div className="flex flex-col items-baseline uppercase ml-5">
                             <span className="text-xs text-brand-light-gray">Step {index+1}</span>
@@ -119,26 +120,16 @@ export default function Form({}: FormProps) {
                         { errors.name && <span className=" text-sm font-bold text-brand-strawberry-red">{errors.name.message}</span> }
                     </div>
                     <input type="text" id="name" placeholder="e.g. Stephen King" 
-                        {...register("name",
-                            // { required: "This field is required" }
-                        )}
+                        {...register("name")}
                     />
                     
                     <div className="flex grow justify-between mt-5 mb-2">
                         <label htmlFor="email">Email Address</label>
-                        { errors.email && <span cpx-6lassName=" text-sm font-bold text-brand-strawberry-red">{errors.email.message}</span> }
+                        { errors.email && <span className=" text-sm font-bold text-brand-strawberry-red">{errors.email.message}</span> }
                     </div>
                     <input type="email" id="email" placeholder="e.g. stephenking@lorem.com" 
-                        {...register("email",
-                            // { required: "This field is required",
-                            //     pattern: {
-                            //         value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                            //         message: "Please enter a valid email address"
-                            //     }
-                            // }
-                        )}
+                        {...register("email")}
                     />
-                    {  }
 
                     <div className="flex grow justify-between mt-5 mb-2">
                         <label htmlFor="phone">Phone Number</label>
@@ -147,10 +138,6 @@ export default function Form({}: FormProps) {
                     <Controller
                         control={control}
                         name="phone"
-                        rules={{ 
-                            // required: "This field is required", 
-                            validate: value => isValidPhoneNumber(value) || "Please enter a valid phone number"
-                        }}
                         render={({ field: { onChange, value } }) => (
                             <PhoneInput
                                 className="phone-input"
@@ -276,20 +263,26 @@ export default function Form({}: FormProps) {
 
                 <div className="flex justify-between mt-auto mb-4">
                     <button 
-                        disabled={currentStep === 0} 
+                        disabled={currentStep === 0 || currentStep === steps.length - 1} 
                         className="flex w-min text-nowrap text-brand-cool-gray my-3 disabled:invisible"
-                        onClick={prev}
+                        onClick={(event) => {
+                            event.preventDefault()
+                            handleNav(currentStep - 1)
+                        }}
                     >Go Back</button>
                     <button 
-                        disabled={currentStep === steps.length - 1} 
-                        className="flex w-min text-nowrap peer bg-brand-marine-blue text-brand-magnolia font-medium py-3 px-6 rounded-lg disabled:hidden"
-                        onClick={next}
+                        disabled={currentStep >= steps.length - 2} 
+                        className="flex w-min text-nowrap bg-brand-marine-blue text-brand-magnolia font-medium py-3 px-6 rounded-lg disabled:hidden"
+                        onClick={(event) => {
+                            event.preventDefault()
+                            handleNav(currentStep + 1)
+                        }}
                     >Next Step</button>
                     <button 
-                        disabled={currentStep !== steps.length - 1}    // FIXME: inconsistent ugly logic
+                        disabled={currentStep !== steps.length - 2}    // FIXME: form doesnt submit
                         type="submit" 
-                        className="flex w-min text-nowrap peer bg-brand-marine-blue text-brand-magnolia font-medium py-3 px-6 rounded-lg peer-enabled:hidden"
-                    >Submit</button>
+                        className="flex w-min text-nowrap bg-brand-marine-blue text-brand-magnolia font-medium py-3 px-6 rounded-lg disabled:hidden"
+                    >Confirm</button>
                 </div>
             </form>
         </div>
--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,9 +1,12 @@
+import { isValidPhoneNumber } from 'react-phone-number-input'
 import { z } from 'zod'
 
 export const formDataSchema = z.object({
     name: z.string().min(1, 'This field is required'),
     email: z.string().min(1, 'This field is required').email('Please enter a valid email address'),
-    phone: z.string().min(1, 'This field is required'),
+    phone: z.string().min(1, 'This field is required').refine(value => isValidPhoneNumber(value), {
+        message: "Please enter a valid phone number"
+    }),
     plan: z.enum(["arcade", "advanced", "pro"]),
     billing: z.enum(["montly", "yearly"]),
     addons: z.object({
