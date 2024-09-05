4a35d3eb0a6c564eb7ecd3668a79b875751fac5f
JavierDeSant <j.desantiagosoto@gmail.com>
2024-09-05 17:38:19 +0200

    fix(ToggleSwitch): make buttons set one state only

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -4,22 +4,31 @@ type ToggleSwitchProps = {
     label2: string
 }
 
+const handleCheck = (state: boolean) => {
+    const toggleSwitch = document.getElementById("switch") as HTMLInputElement
+    if (toggleSwitch) {
+        toggleSwitch.checked = state
+    }
+}
+
 export default function ToggleSwitch({ value, label1, label2 }: ToggleSwitchProps) {
   return (
     <div className="flex">
-        <button id="checker1" role="label"
-            className="font-medium text-base"
+        <button id="checker1" type="button" role="label"
+            className="font-medium text-base focus:outline-none focus:outline-brand-purplish-blue"
+            onClick={() => handleCheck(false)}
         >{label1}</button>
         <label className="cursor-pointer">
-            <input type="checkbox" value={value} className="sr-only peer" />
+            <input type="checkbox" id="switch" value={value} className="sr-only peer" />
             <div className="relative w-12 h-6 mx-6 bg-brand-marine-blue
                 peer-focus:outline-none peer-focus:outline-brand-purplish-blue peer-focus:ring-blue-300 rounded-full peer 
                 dark:bg-brand-marine-blue peer-checked:after:translate-x-[20px] rtl:peer-checked:after:-translate-x-[24px] peer-checked:after:border-white 
                 after:absolute after:top-[4px] after:start-[6px] after:bg-white after:border-gray-300 after:border after:rounded-full 
                 after:h-4 after:w-4 after:transition-all" />
         </label>
-        <button id="checker2" role="label"
-            className="font-medium text-base"
+        <button id="checker2" type="button" role="label"
+            className="font-medium text-base focus:outline-none focus:outline-brand-purplish-blue"
+            onClick={() => handleCheck(true)}
         >{label2}</button>
     </div>
   )
