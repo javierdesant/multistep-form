71ed269b4c1f42e166664adab22f341a4a0b8c37
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-09-11 12:02:54 +0200

    perf(step-1): replace margin with flexbox gap

--- src/components/form/Step1.tsx
+++ src/components/form/Step1.tsx
@@ -17,60 +17,67 @@ export default function Step1() {
         Please provide your name, email address, and phone number.
       </p>
 
-      <div className="mb-2 flex grow justify-between">
-        <label htmlFor="name">Name</label>
-        {errors.name && (
-          <span className="text-xs font-bold text-brand-strawberry-red md:text-sm">
-            {errors.name.message}
-          </span>
-        )}
-      </div>
-      <input
-        type="text"
-        id="name"
-        placeholder="e.g. Stephen King"
-        className={errors.name && "border-brand-strawberry-red"}
-        {...register("name")}
-      />
-
-      <div className="mb-2 mt-5 flex grow justify-between">
-        <label htmlFor="email">Email Address</label>
-        {errors.email && (
-          <span className="text-xs font-bold text-brand-strawberry-red md:text-sm">
-            {errors.email.message}
-          </span>
-        )}
-      </div>
-      <input
-        type="email"
-        id="email"
-        placeholder="e.g. stephenking@lorem.com"
-        className={errors.email && "border-brand-strawberry-red"}
-        {...register("email")}
-      />
-
-      <div className="mb-2 mt-5 flex grow justify-between">
-        <label htmlFor="phone">Phone Number</label>
-        {errors.phone && (
-          <span className="text-xs font-bold text-brand-strawberry-red md:text-sm">
-            {errors.phone.message}
-          </span>
-        )}
-      </div>
-
-      <Controller
-        control={control}
-        name="phone"
-        render={({ field: { onChange, value } }) => (
-          <PhoneInput
-            className={`phone-input ${errors.phone && "*:border-brand-strawberry-red"}`}
-            value={value}
-            onChange={onChange}
-            defaultCountry="US"
-            placeholder="e.g. +1 234 567 890"
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-1">
+          <div className="flex grow justify-between">
+            <label htmlFor="name">Name</label>
+            {errors.name && (
+              <span className="text-xs font-bold text-brand-strawberry-red md:text-sm">
+                {errors.name.message}
+              </span>
+            )}
+          </div>
+          <input
+            type="text"
+            id="name"
+            placeholder="e.g. Stephen King"
+            className={errors.name && "border-brand-strawberry-red"}
+            {...register("name")}
           />
-        )}
-      />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <div className="flex grow justify-between">
+            <label htmlFor="email">Email Address</label>
+            {errors.email && (
+              <span className="text-xs font-bold text-brand-strawberry-red md:text-sm">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+          <input
+            type="email"
+            id="email"
+            placeholder="e.g. stephenking@lorem.com"
+            className={errors.email && "border-brand-strawberry-red"}
+            {...register("email")}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <div className="flex grow justify-between">
+            <label htmlFor="phone">Phone Number</label>
+            {errors.phone && (
+              <span className="text-xs font-bold text-brand-strawberry-red md:text-sm">
+                {errors.phone.message}
+              </span>
+            )}
+          </div>
+          <Controller
+            control={control}
+            name="phone"
+            render={({ field: { onChange, value } }) => (
+              <PhoneInput
+                className={`phone-input ${errors.phone && "*:border-brand-strawberry-red"}`}
+                value={value}
+                onChange={onChange}
+                defaultCountry="US"
+                placeholder="e.g. +1 234 567 890"
+              />
+            )}
+          />
+        </div>
+      </div>
     </div>
   );
 }
