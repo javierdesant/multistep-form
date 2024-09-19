8d8634cca4959f744bee63762d0462e917a8c92a
JavierDeSant <j.desantiagosoto@gmail.com>
2024-09-20 01:12:58 +0200

    docs: create custom README and upload screenshots

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -32,7 +32,12 @@ export default function ToggleSwitch({
           className="peer sr-only"
           onChange={(e) => onChange(e.target.checked ? "yearly" : "monthly")}
         />
-        <div className="peer relative mx-6 h-5 w-10 rounded-full bg-brand-marine-blue after:absolute after:start-[6px] after:top-[4px] after:h-3 after:w-3 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all peer-checked:after:translate-x-[17px] peer-checked:after:border-white peer-focus:outline-none peer-focus:outline-brand-purplish-blue peer-focus:ring-blue-300 md:h-6 md:w-12 md:after:h-4 md:after:w-4 md:peer-checked:after:translate-x-[20px] rtl:peer-checked:after:-translate-x-[24px] dark:bg-brand-marine-blue" />
+        <div className="peer relative mx-6 h-5 w-10 rounded-full bg-brand-marine-blue 
+          after:absolute after:start-[6px] after:top-[4px] after:h-3 after:w-3 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all 
+          peer-checked:after:translate-x-[17px] peer-checked:after:border-white peer-focus:outline-none 
+          peer-focus:outline-brand-purplish-blue peer-focus:ring-blue-300 
+          md:h-6 md:w-12 md:after:h-4 md:after:w-4 md:peer-checked:after:translate-x-[20px] 
+          rtl:peer-checked:after:-translate-x-[24px] dark:bg-brand-marine-blue" />
       </label>
       <button
         id="checker2"
